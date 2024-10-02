ce251c55adb25c91a44f0f125de1e9612ffad3e4
Joshi-Manmohan <mannuj039@gmail.com>
2024-10-02 22:09:24 +0530

    factral'

---
+++ factoral.py
@@ -0,0 +1,9 @@
+# This program calculates the factorial of a number
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n - 1)
+
+number = int(input("Enter a number to calculate its factorial: "))
+print(f"The factorial of {number} is {factorial(number)}")
